22db169b3267669724c460fc922e362ca1588585
poojan <poojan.d.sharma@gmail.com>
2023-09-19 10:43:19 +0800

    experience CRUD api callbacks

--- app/models.py
+++ app/models.py
@@ -4,25 +4,16 @@ class Experience(db.Model):
     __tablename__ = 'tbl_experience'
     __table_args__ = {'extend_existing': True}  # Ensure table isn't recreated
 
-    experienceId = db.Column(db.Integer, primary_key=True)
-    studentId = db.Column(db.Integer)
-    jobTitle = db.Column(db.String(250))
-    fromDate = db.Column(db.DateTime)
-    toDate = db.Column(db.DateTime)
+    experience_id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer)
+    job_title = db.Column(db.String(250))
+    from_date = db.Column(db.DateTime)
+    to_date = db.Column(db.DateTime)
     experience = db.Column(db.Text)
 
     def __init__(self, studentId, jobTitle, fromDate, toDate, experience):
-        self.studentId = studentId
-        self.jobTitle = jobTitle
-        self.fromDate = fromDate
-        self.toDate = toDate
+        self.student_id = studentId
+        self.job_title = jobTitle
+        self.from_date = fromDate
+        self.to_date = toDate
         self.experience = experience
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80))
-    email = db.Column(db.String(120), unique=True)
-
-    def __init__(self, name, email):
-        self.name = name
-        self.email = email
--- app/views.py
+++ app/views.py
@@ -1,47 +1,63 @@
 from flask import request, jsonify
 from app import app, db, ma
-from app.models import User
+from app.models import Experience
 
-class UserSchema(ma.SQLAlchemyAutoSchema):
+class ExperienceSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
-        model = User
+        model = Experience
         sqla_session = db.session
 
-user_schema = UserSchema()
-users_schema = UserSchema(many=True)
+experience_schema = ExperienceSchema()
+experiences_schema = ExperienceSchema(many=True)
 
-@app.route('/user', methods=['POST'])
-def add_user():
-    name = request.json['name']
-    email = request.json['email']
-    new_user = User(name, email)
-    db.session.add(new_user)
+@app.route('/experience', methods=['POST'])
+def add_experience():
+    studentId = request.json['studentId']
+    jobTitle = request.json['jobTitle']
+    fromDate = request.json['fromDate']
+    toDate = request.json['toDate']
+    experience = request.json['experience']
+
+    new_experience = Experience(studentId, jobTitle, fromDate, toDate, experience)
+    db.session.add(new_experience)
     db.session.commit()
-    return user_schema.jsonify(new_user)
+    return experience_schema.jsonify(new_experience)
 
-@app.route('/user', methods=['GET'])
-def get_users():
-    all_users = User.query.all()
-    return users_schema.jsonify(all_users)
+@app.route('/experience', methods=['GET'])
+def get_experiences():
+    all_experiences = Experience.query.all()
+    return experiences_schema.jsonify(all_experiences)
 
-@app.route('/user/<id>', methods=['GET'])
-def get_user(id):
-    user = User.query.get(id)
-    return user_schema.jsonify(user)
+@app.route('/experience/<id>', methods=['GET'])
+def get_experience(id):
+    experience = Experience.query.get(id)
+    if not experience:
+        return jsonify({"error": "Experience not found"}), 404
+    else:
+        return experience_schema.jsonify(experience)
 
-@app.route('/user/<id>', methods=['PUT'])
-def update_user(id):
-    user = User.query.get(id)
-    name = request.json['name']
-    email = request.json['email']
-    user.name = name
-    user.email = email
-    db.session.commit()
-    return user_schema.jsonify(user)
+@app.route('/experience/<id>', methods=['PUT'])
+def update_experience(id):
+    experience = Experience.query.get(id)
 
-@app.route('/user/<id>', methods=['DELETE'])
-def delete_user(id):
-    user = User.query.get(id)
-    db.session.delete(user)
-    db.session.commit()
-    return user_schema.jsonify(user)
+    if not experience:
+        return jsonify({"error": "Experience not found"}), 404
+    else:
+        experience.student_id = request.json.get('studentId', experience.student_id)
+        experience.job_title = request.json.get('jobTitle', experience.job_title)
+        experience.from_date = request.json.get('fromDate', experience.from_date)
+        experience.to_date = request.json.get('toDate', experience.to_date)
+        experience.experience = request.json.get('experience', experience.experience)
+
+        db.session.commit()
+        return experience_schema.jsonify(experience)
+
+@app.route('/experience/<id>', methods=['DELETE'])
+def delete_experience(id):
+    experience = Experience.query.get(id)
+    if not experience:
+        return jsonify({"error": "Experience not found"}), 404
+    else:
+        db.session.delete(experience)
+        db.session.commit()
+        return experience_schema.jsonify(experience)
